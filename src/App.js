a35d3f02a3d2d68a9bb95f968204004ee10c52a7
Ayman Omer <aymangebrilomer@gmail.com>
2021-03-16 19:17:52 -0400

    setting up the Files and make src files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hey again </Text>
+      <Text>How are you </Text>
       <StatusBar style="auto" />
     </View>
   );
