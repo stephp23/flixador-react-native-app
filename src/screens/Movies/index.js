67af026639c066d9a91928a5fbd5f1f80e6fb581
stephp23 <stephp23@gmail.com>
2021-03-22 15:22:16 -0400

    movies page api call for most popular movies section (row 1 of page)

--- src/screens/Movies/index.js
+++ src/screens/Movies/index.js
@@ -1,7 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import config from "../../../config";
+import axios from "axios";
+import Nav from "../components/Nav/Nav";
+
+
+const imgUrl = "https://image.tmdb.org/t/p/original";
 
 const Movies = () => {
+
+  const { movies } = useSelector((state) => state.movies);
+  const text = useSelector((state) => state.movies.text);
+
+  const [moviesPagesRow1, setMoviesPagesRow1] = useState([]);
+
+  const [show, setShow] = useState(true);
+
+  const fetchMoviesPageRow1 = async () => {
+    let URL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&with_genres=28`;
+    const response = await axios.get(URL);
+    setMoviesPagesRow1(response.data.results);
+  };
+
+  useEffect(() => {
+    fetchMoviesPageRow1();
+  }, []);
+
+  useEffect(() => {
+    const handleScrolling = window.addEventListener("scroll", () => {
+      window.scrollY > -20 ? setShow(true) : setShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", handleScrolling);
+    };
+  }, []);
+
+
   return (
     <View style={styles.root}>
       <Text>this is movies</Text>
