3b0deccd44b78400356626045e458cd92d075529
Leesel Fraser <leeselfraser08@gmail.com>
2021-03-26 20:12:53 -0400

    update team page

--- src/screens/Team/index.js
+++ src/screens/Team/index.js
@@ -1,83 +1,11 @@
 import React from "react";
 import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+
+
 const { width, height } = Dimensions.get('screen');
 
 
 // import "./styleTeam";
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     paddingTop: 50,
-//   },
-//   stretch: {
-//     width: 50,
-//     height: 200,
-//     resizeMode: 'stretch',
-//   },
-// });
-
-// const Team = () => {
-//   return (
-
-//     <View>
-//       <ScrollView>
-//         <Text style={styles.text}>
-//           Leesel Fraser
-//       </Text>
-//         <Image
-//           style={styles.row_poster}
-//           source={require('../../../assets/leesel.png')}
-//         />
-//         <Text style={styles.text}>
-//           Stephanie Pena
-//       </Text>
-//         <Image
-//           style={styles.row_poster}
-//           source={require('../../../assets/stephanie.png')}
-//         />
-//         <Text style={styles.text}>
-//           Katherine Fernandez
-//       </Text>
-//         <Image
-//           style={styles.row_poster}
-//           source={require('../../../assets/katherine.png')}
-//         />
-//         <Text style={styles.text}>
-//           Ayman Gebril
-//       </Text>
-//         <Image
-//           style={styles.row_poster}
-//           source={require('../../../assets/ayman.png')}
-//         />
-//       </ScrollView>
-//     </View>
-//   )
-// };
-
-// export default Team;
-
-// const data = [
-//   'https://i.redd.it/bjqhu3ax2cz01.jpg',
-//   'https://graffiti-artist.net/wp-content/uploads/2019/02/nyc-graffiti-expedia.jpg',
-//   'https://assets.mycast.io/posters/adventures-in-babysitting-2022-fan-casting-poster-24041-large.jpg?1577835849',
-//   'https://mir-s3-cdn-cf.behance.net/project_modules/disp/add637105058085.5f70f03e2a6c0.png'
-
-// ];
-
-// const data = [
-//   require('../../../assets/stephanie.png'),
-//   require('../../../assets/leesel.png'),
-//   require('../../../assets/ayman.png'),
-//   require('../../../assets/katherine.png')
-// ];
-
-// const names = [
-//   'Stephanie Pena',
-//   'Leesel Fraser',
-//   'Ayman',
-//   'katherine'
-// ];
 
 const data = [
   {
